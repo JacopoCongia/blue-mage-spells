6b21d133d4035567ae4b7e16af13b2cfefffafc9
jacopo.congia <jacopo.congia@gmail.com>
2023-06-07 17:06:24 +0200

    Testing viewport width

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -10,7 +10,7 @@ export default function App({ Component, pageProps }) {
       <Head>
         <meta
           name="viewport"
-          content="width=device-width, initial-scale=1"
+          content="width=device-width"
         />
       </Head>
       <ThemeProvider>
